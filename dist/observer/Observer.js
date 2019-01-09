6d426d3853b83a0e1042b2822ed80a3c9db9ff7f
xiang <313250678@qq.com>
2019-01-10 00:26:41 +0800

    20190110002640_xiang_feat_移除DIST追踪

--- dist/observer/Observer.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Observer {
-    constructor() { }
-    update(context) {
-        console.log('no anyting to update');
-    }
-}
-exports.default = Observer;
